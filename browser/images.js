92a6cdf2a476302703a379b79443743dc11fcf7d
Tim Hudson <tim.hudson15@gmail.com>
2014-06-01 20:31:59 -0400

    Use random querystring to keep same sized images unique

--- browser/images.js
+++ browser/images.js
@@ -1,28 +1,35 @@
 /** @jsx React.DOM */
 var React = require('react')
 var Image = require('./components/image')
+var crypto = require('crypto')
 
 module.exports = React.createClass({
   getInitialState: function() {
+    var count = 21
+    var images = []
+    var aspectRatio = window.innerWidth / window.innerHeight
+    var width = 480
+    var height = Math.round(width / aspectRatio)
+
+    while (images.length < 21) {
+      images.push('/' + width + 'x' + height + '.jpg?s=' + crypto.randomBytes(5).toString('hex'))
+    }
+
     return {
-      count: 21
+      images: images,
+      aspectRatio: aspectRatio
     }
   },
   render: function() {
-    var images = []
-    var aspectRatio = window.innerWidth / window.innerHeight
-    var width, height
+    var self = this
 
-    while (images.length < 21) {
-      width = 480 + images.length
-      height = Math.round(width / aspectRatio)
-
-      images.push(
+    var images = this.state.images.map(function(image) {
+      return (
         <div className="image-container">
-          <Image src={'/' + width + 'x' + height + '.jpg'} aspectRatio={aspectRatio} />
+          <Image src={image} aspectRatio={self.state.aspectRatio} />
         </div>
       )
-    }
+    })
 
     return <div className="group">{images}</div>
   }
